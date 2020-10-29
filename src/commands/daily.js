c07112524e5e833fb0e3b1fb8e6a5ba6eeb78f38
Patrick <65928079+HippoBaguette@users.noreply.github.com>
2020-10-29 19:04:02 +0800

    Update daily.js

--- src/commands/daily.js
+++ src/commands/daily.js
@@ -3,16 +3,17 @@ module.exports = {
   description: 'Daily xp bonus',
   execute(message, talkedRecently, db){
     if (talkedRecently.has(message.author.id)) {
-      message.channel.send("Please wait 24 hours after the last time you executed this command");
+      message.channel.send("Please wait 24 hours after the last time you sent this command");
       console.log(talkedRecently);
     } else {
 
-      db[message.author.id].xp += (db[message.author.id].xp * 0.5 - 1);
-      message.channel.send('You have succesfully claimed your daily XP. come back tommorow for more!');
+      db[message.author.id].xp += (db[message.author.id].xp * 0.05);
+
+
+      message.channel.send('Claimed!');
 
       talkedRecently.add(message.author.id);
       setTimeout(() => {
-        // Removes the user from the set after a minute
         talkedRecently.delete(message.author.id);
       }, 86400000);
     }
